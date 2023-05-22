67a0af0d04f1699bd17c557459b0b3a689f5c226
JipauDev <51697654+JipauDev@users.noreply.github.com>
2023-05-22 13:56:48 +0700

    Update modal_event.dart

--- lib/modal/modal_event.dart
+++ lib/modal/modal_event.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Event {
   String? category;
-  String? date;
+  Timestamp? date;
   String? description;
   String? id;
   String? location;
@@ -14,13 +15,13 @@ class Event {
   String? type;
   String? userDesc;
   String? userName;
+  String? time;
   String? userProfile;
   String? image;
-  String? image2;
-  String? image3;
-  String? image4;
-  String? image5;
   int? ticket;
+  int? count;
+  LatLng? latLng;
+
 
   Event(
       {this.category,
@@ -28,38 +29,39 @@ class Event {
       this.description,
       this.id,
       this.location,
+      this.time,
       this.mapsLangLink,
       this.mapsLatLink,
       this.price,
       this.title,
       this.type,
-      this.image2,
-      this.image3,
-      this.image4,
-      this.image5,
       this.userDesc,
       this.ticket,
       this.userName,
       this.image,
+      this.count,
+      this.latLng,
       this.userProfile});
 
   factory Event.fromFirestore(DocumentSnapshot snapshot){
     Map data = snapshot.data() as Map<dynamic, dynamic>;
+    
+    
+  LatLng latLng = LatLng(data['mapsLatLink'], data['mapsLangLink']);
     return Event(
        category : data['category'],
     date  : data['date'],
     image: data['image'],
     description  : data['description'],
     id  : data['id'],
+    latLng: latLng,
+    count: data['count'],
     location  : data['location'],
+    time: data['time'],
     mapsLangLink  : data['mapsLangLink'],
     mapsLatLink  : data['mapsLatLink'],
     price  : data['price'],
     title  : data['title'],
-    image2: data['image2'],
-    image3: data['image3'],
-    image4: data['image4'],
-    image5: data['image5'],
     type  : data['type'],
     ticket: data['ticket'],
     userDesc  : data['userDesc'],
@@ -70,55 +72,55 @@ class Event {
 }
 
 
-class Event2 {
-  String? category;
-  String? date;
-  String? description;
-  String? id;
-  String? location;
-  double? mapsLangLink;
-  double? mapsLatLink;
-  int? price;
-  String? title;
-  String? type;
-  String? userDesc;
-  String? userName;
-  String? userProfile;
-  String? image;
+// class Event2 {
+//   String? category;
+//   Timestamp? date;
+//   String? description;
+//   String? id;
+//   String? location;
+//   double? mapsLangLink;
+//   double? mapsLatLink;
+//   int? price;
+//   String? title;
+//   String? type;
+//   String? userDesc;
+//   String? userName;
+//   String? userProfile;
+//   String? image;
 
-  Event2(
-      {this.category,
-      this.date,
-      this.description,
-      this.id,
-      this.location,
-      this.mapsLangLink,
-      this.mapsLatLink,
-      this.price,
-      this.title,
-      this.type,
-      this.userDesc,
-      this.userName,
-      this.image,
-      this.userProfile});
+//   Event2(
+//       {this.category,
+//       this.date,
+//       this.description,
+//       this.id,
+//       this.location,
+//       this.mapsLangLink,
+//       this.mapsLatLink,
+//       this.price,
+//       this.title,
+//       this.type,
+//       this.userDesc,
+//       this.userName,
+//       this.image,
+//       this.userProfile});
 
-  factory Event2.fromFirestore(DocumentSnapshot snapshot){
-    Map data = snapshot.data() as Map<dynamic, dynamic>;
-    return Event2(
-       category : data['category'],
-    date  : data['date'],
-    image: data['image'],
-    description  : data['description'],
-    id  : data['id'],
-    location  : data['location'],
-    mapsLangLink  : data['mapsLangLink'],
-    mapsLatLink  : data['mapsLatLink'],
-    price  : data['price'],
-    title  : data['title'],
-    type  : data['type'],
-    userDesc  : data['userDesc'],
-    userName  : data['userName'],
-    userProfile  : data['userProfile'],
-    );
-  }
-}
+//   factory Event2.fromFirestore(DocumentSnapshot snapshot){
+//     Map data = snapshot.data() as Map<dynamic, dynamic>;
+//     return Event2(
+//        category : data['category'],
+//     date  : data['date'],
+//     image: data['image'],
+//     description  : data['description'],
+//     id  : data['id'],
+//     location  : data['location'],
+//     mapsLangLink  : data['mapsLangLink'],
+//     mapsLatLink  : data['mapsLatLink'],
+//     price  : data['price'],
+//     title  : data['title'],
+//     type  : data['type'],
+//     userDesc  : data['userDesc'],
+//     userName  : data['userName'],
+//     userProfile  : data['userProfile'],
+//     );
+//   }
+// }
