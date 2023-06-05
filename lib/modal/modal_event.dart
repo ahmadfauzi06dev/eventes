6138cb949c62c1cbcb4335c35642109f0f41e430
JipauDev <51697654+JipauDev@users.noreply.github.com>
2023-06-05 15:51:44 +0700

    Update modal_event.dart

--- lib/modal/modal_event.dart
+++ lib/modal/modal_event.dart
@@ -21,17 +21,21 @@ class Event {
   int? ticket;
   int? count;
   LatLng? latLng;
+  Map<String, dynamic>? joinEvent;
+  String? typePayment;
 
 
   Event(
       {this.category,
       this.date,
       this.description,
+    this.joinEvent,
       this.id,
       this.location,
       this.time,
       this.mapsLangLink,
       this.mapsLatLink,
+      this.typePayment,
       this.price,
       this.title,
       this.type,
@@ -53,10 +57,11 @@ class Event {
     date  : data['date'],
     image: data['image'],
     description  : data['description'],
-    id  : data['id'],
+    id  : snapshot.id,
     latLng: latLng,
     count: data['count'],
     location  : data['location'],
+    typePayment: data['typePayment'],
     time: data['time'],
     mapsLangLink  : data['mapsLangLink'],
     mapsLatLink  : data['mapsLatLink'],
@@ -67,60 +72,8 @@ class Event {
     userDesc  : data['userDesc'],
     userName  : data['userName'],
     userProfile  : data['userProfile'],
+      joinEvent: data['joinEvent'] ?? {},
     );
   }
 }
 
-
-// class Event2 {
-//   String? category;
-//   Timestamp? date;
-//   String? description;
-//   String? id;
-//   String? location;
-//   double? mapsLangLink;
-//   double? mapsLatLink;
-//   int? price;
-//   String? title;
-//   String? type;
-//   String? userDesc;
-//   String? userName;
-//   String? userProfile;
-//   String? image;
-
-//   Event2(
-//       {this.category,
-//       this.date,
-//       this.description,
-//       this.id,
-//       this.location,
-//       this.mapsLangLink,
-//       this.mapsLatLink,
-//       this.price,
-//       this.title,
-//       this.type,
-//       this.userDesc,
-//       this.userName,
-//       this.image,
-//       this.userProfile});
-
-//   factory Event2.fromFirestore(DocumentSnapshot snapshot){
-//     Map data = snapshot.data() as Map<dynamic, dynamic>;
-//     return Event2(
-//        category : data['category'],
-//     date  : data['date'],
-//     image: data['image'],
-//     description  : data['description'],
-//     id  : data['id'],
-//     location  : data['location'],
-//     mapsLangLink  : data['mapsLangLink'],
-//     mapsLatLink  : data['mapsLatLink'],
-//     price  : data['price'],
-//     title  : data['title'],
-//     type  : data['type'],
-//     userDesc  : data['userDesc'],
-//     userName  : data['userName'],
-//     userProfile  : data['userProfile'],
-//     );
-//   }
-// }
